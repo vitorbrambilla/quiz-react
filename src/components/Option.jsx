f979f7f88e5ce8f3a9b772688ce6a5af6b097653
Vitor <vitor.reis@crmservices.com.br>
2022-09-12 10:38:29 -0300

    first commit

---
+++ src/components/Option.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import "./Option.css";
+
+function Option({ option, selectOption, answer }) {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div
+      className={`option ${
+        quizState.answerSelected && option === answer ? "correct" : ""
+      } ${
+        quizState.answerSelected && option !== answer ? "wrong" : ""
+      }`}
+      onClick={() => selectOption()}
+    >
+      <p>{option}</p>
+    </div>
+  );
+}
+
+export default Option;
